0a03e6e08c5258852e0c1d59493296ddedffcad6
Loay Asklany <kenzon7g@gmail.com>
2016-04-15 16:34:24 +0200

    Running
    Add hab btn, add hab count btn

---
+++ js/Daily.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {});
+
+var $counter = 0;
+$('.newhabbt').click(function () {
+
+});
+
+
+$(document).on('click', '#add', function () {
+	var x = $(this).find("div>div>h3").text();
+	$(this).find("div>div>h3").text(Number(x) + 1);
+	return false;
+});
+
+
+$('.nh').click(function () {
+	var nhtxt = $('#nhtxt').val();
+	$counter = $counter + 1;
+	$('div#bigbox').append($("<a/>", {
+		href: "#",
+		id: "add"
+	}).append($('<div/>', {
+			'class': 'row'
+		})
+		.append($('<div/>', {
+				'class': 'col-xs-10'
+			})
+			.append($('<h2/>', {
+				text: nhtxt
+			})))
+
+		.append($('<div/>', {
+				'class': 'col-xs-2'
+			})
+			.append($('<h3/>', {
+				'class': "count",
+				"id": "hab" + $counter,
+				text: '0'
+			})))
+	));
+
+});
